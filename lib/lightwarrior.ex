8243292f7d9303e8733fc4e71c13aa8675264aee
spezi <peter.skamrahl@telekom.de>
2024-06-18 00:51:24 +0200

    stripe details

--- lib/lightwarrior.ex
+++ lib/lightwarrior.ex
@@ -7,7 +7,30 @@ defmodule Lightwarrior do
   if it comes from the database, an external API or others.
   """
 
+  alias Lightwarrior.Helper
+
   require Math
+
+  def get_led_size(stripe_data, mapping_container_size) do
+    dbg(stripe_data)
+    firstLed = Enum.fetch!(Map.get(stripe_data, "leds"), 0)
+    dbg(firstLed)
+    point = {
+      (firstLed["hmax"] - firstLed["hmin"]),
+      (firstLed["vmax"] - firstLed["vmin"])
+    }
+    %{
+      pixel: %{
+        width: Helper.coordinate_to_pixel(elem(point, 0), mapping_container_size.width),
+        height: Helper.coordinate_to_pixel(elem(point, 1), mapping_container_size.height)
+      },
+      point: %{
+        width: elem(point, 0),
+        height: elem(point, 1)
+      },
+      mapping_container_size: mapping_container_size
+    }
+  end
 
   def update_selected_stripe_data_pixel(leds_pixel, selected, points) do
     #dbg(selected)
@@ -34,20 +57,22 @@ defmodule Lightwarrior do
     #bounds.minY
     #bounds.maxY
 
-    max_size = get_distance(points)/num_leds
+    mapping_container_size = get_distance(points)/num_leds
 
 
     step_x = (points.end.x - points.start.x) / (num_leds - 1)
     step_y = (points.end.y - points.start.y) / (num_leds - 1)
 
     #for i <- 0..(num_leds - 1) do
-    #  %{:vmin => y1 + i * step_y, :hmin => x1 + i * step_x, :vmax => y1 + i * step_y + max_size, :hmax => x1 + i * step_x + max_size }
+    #  %{:vmin => y1 + i * step_y, :hmin => x1 + i * step_x, :vmax => y1 + i * step_y + mapping_container_size, :hmax => x1 + i * step_x + mapping_container_size }
     #end
 
     for i <- 0..(num_leds - 1) do
-      %{"vmin" => points.start.y + i * step_y, "hmin" => points.start.x + i * step_x, "vmax" => points.start.y + i * step_y + max_size, "hmax" => points.start.x + i * step_x + max_size }
+      %{"vmin" => points.start.y + i * step_y, "hmin" => points.start.x + i * step_x, "vmax" => points.start.y + i * step_y + mapping_container_size, "hmax" => points.start.x + i * step_x + mapping_container_size }
     end
 
   end
+
+
 
 end
