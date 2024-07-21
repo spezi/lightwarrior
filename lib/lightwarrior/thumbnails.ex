748d2dec90fe14a8ab75d0d338bb7494a524af8a
spezi <peter.skamrahl@telekom.de>
2024-07-21 02:39:15 +0200

    init sequencer

---
+++ lib/lightwarrior/thumbnails.ex
@@ -0,0 +1,20 @@
+defmodule Lightwarrior.Imageplayer.Thumbnail do
+  require Logger
+
+  @thumbnails_dir "priv/static/thumbnails"
+
+  def generate_thumbnail(input_path) do
+    File.mkdir_p!(@thumbnails_dir)
+
+    output_path = Path.join(@thumbnails_dir, Path.basename(input_path, Path.extname(input_path)) <> "_thumb.jpg")
+
+    case Thumbnex.create_thumbnail(input_path, output_path) do
+      :ok ->
+        Logger.info("Thumbnail created successfully: #{output_path}")
+        {:ok, output_path}
+      {:error, reason} ->
+        Logger.error("Failed to create thumbnail: #{reason}")
+        {:error, reason}
+    end
+  end
+end
